de4b155cf8d069db0515e7b1bc92cea6afba2058
baptiste amanieu <baptiste.amanieu@gmail.com>
2023-10-25 09:29:09 +0200

    cellule lumineuse fonctionne

--- TP3_AMANIEU/LightOff_AMANIEU_version_console/src/lightoff_amanieu_version_console/LightOff_AMANIEU_version_console.java
+++ TP3_AMANIEU/LightOff_AMANIEU_version_console/src/lightoff_amanieu_version_console/LightOff_AMANIEU_version_console.java
@@ -14,28 +14,60 @@ public class LightOff_AMANIEU_version_console {
      * @param args the command line arguments
      */
     public static void main(String[] args)  {
-        // CrÃ©ez une cellule lumineuse par dÃ©faut (Ã©teinte)
+        // Créez une cellule lumineuse par défaut (éteinte)
         CelluleLumineuse cellule1 = new CelluleLumineuse();
 
-        // Affichez l'Ã©tat initial
-        System.out.println("Ã‰tat de la cellule 1 (par dÃ©faut) : " + cellule1.getEtat());
+        // Affichez l'état initial
+        System.out.println("État de la cellule 1 (par défaut) : " + cellule1.getEtat());
 
         // Allumez la cellule
         cellule1.allumer();
-        System.out.println("Ã‰tat de la cellule 1 (aprÃ¨s allumage) : " + cellule1.getEtat());
+        System.out.println("État de la cellule 1 (après allumage) : " + cellule1.getEtat());
 
-        // Ã‰teignez la cellule
+        // Éteignez la cellule
         cellule1.eteindre();
-        System.out.println("Ã‰tat de la cellule 1 (aprÃ¨s extinction) : " + cellule1.getEtat());
+        System.out.println("État de la cellule 1 (après extinction) : " + cellule1.getEtat());
 
-        // CrÃ©ez une deuxiÃ¨me cellule lumineuse (allumÃ©e)
+        // Créez une deuxième cellule lumineuse (allumée)
         CelluleLumineuse cellule2 = new CelluleLumineuse();
 
-        // Allumez la deuxiÃ¨me cellule
+        // Allumez la deuxième cellule
         cellule2.allumer();
-        System.out.println("Ã‰tat de la cellule 2 (allumÃ©e) : " + cellule2.getEtat());
+        System.out.println("État de la cellule 2 (allumée) : " + cellule2.getEtat());
+      // Créez une nouvelle grille de cellules avec 3 lignes et 4 colonnes
+        GrilleDeCellules grille = new GrilleDeCellules(4, 4);
+
+        System.out.println("Grille initiale :");
+        System.out.println(grille);
+
+        grille.activerLigneDeCellules(1);
+        System.out.println("Grille après activation de la ligne 1 :");
+        System.out.println(grille);
+
+        grille.activerColonneDeCellules(2);
+        System.out.println("Grille après activation de la colonne 2 :");
+        System.out.println(grille);
+
+        grille.activerDiagonaleDescendante();
+        System.out.println("Grille après activation de la diagonale descendante :");
+        System.out.println(grille);
+
+        grille.activerDiagonaleMontante();
+        System.out.println("Grille après activation de la diagonale montante :");
+        System.out.println(grille);
+
+        grille.activerLigneColonneOuDiagonaleAleatoire();
+        System.out.println("Grille après activation aléatoire :");
+        System.out.println(grille);
+
+        grille.melangerMatriceAleatoirement(5);
+        System.out.println("Grille après mélange aléatoire (5 tours) :");
+        System.out.println(grille);
+    }
+    
+    
     }
 
-    }
+   
     
 
